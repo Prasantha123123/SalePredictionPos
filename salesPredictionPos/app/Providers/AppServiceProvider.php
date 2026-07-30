3e6be2d075105970b3f6d0611edf1f3fb3caffae
Raman Prasantha <131949176+Prasantha123123@users.noreply.github.com>
2026-07-30 04:36:00 -0700

    ssl error fix and downgrade the composer

--- salesPredictionPos/app/Providers/AppServiceProvider.php
+++ salesPredictionPos/app/Providers/AppServiceProvider.php
@@ -24,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+        }
         $this->configureDefaults();
     }
 
